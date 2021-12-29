b1826b81650062cabe2a1e06c1ffbf89369b3596
Kim Taenam <cjb940103@gmail.com>
2021-12-29 16:03:01 +0900

    url-loader add 후 적용

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,11 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg|webp)$/i,
-                loader: "file-loader",
+                loader: "url-loader",
                 options: {
-                    publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
-                    name: "[name].[ext]?[hash]", // 파일명 형식
+                    publicPath: "./dist/",
+                    name: "[name].[ext]?[hash]",
+                    limit: 20000
                 },
             },
         ]
