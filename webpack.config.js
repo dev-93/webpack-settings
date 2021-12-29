df3da6d2bf5c884f310f9ee8aac854e4b69e3e0e
Kim Taenam <cjb940103@gmail.com>
2021-12-29 14:55:20 +0900

    file loader add 후 settings

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,14 @@ module.exports = {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp)$/i,
+                loader: "file-loader",
+                options: {
+                    publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+                    name: "[name].[ext]?[hash]", // 파일명 형식
+                },
+            },
         ]
     }
 }
