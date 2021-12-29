3af3cb72c0b2b54b5552a8068a04336fe0d3a9f6
Kim Taenam <cjb940103@gmail.com>
2021-12-29 18:21:20 +0900

    css-loader, style-loader, file-loader add 후 적용

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,21 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"], // style-loader를 앞에 추가한다
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: "file-loader",
+        options: {
+          publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+          name: "[name].[ext]?[hash]", // 파일명 형식
+        },
+      }
+    ]
   }
 }
