cbaaa11353858a7d3403ac70d19e020553b3740d
kimtaenam <cjb940103@gmail.com>
2022-01-05 01:18:42 +0900

    sass 문제 가져오기

--- babel.config.js
+++ babel.config.js
@@ -5,13 +5,14 @@ module.exports = {
       {
         targets: {
           chrome: "79",
-          ie: '11',
+          ie: "11" // npm i regenerator-runtime 필요함
         },
-        useBuiltIns: "usage",
+        useBuiltIns: "usage", // 폴리필 사용 방식 지정
         corejs: {
-          version: 2,
-        },
-      },
-    ],
-  ],
-}
+          // 폴리필 버전 지정
+          version: 2
+        }
+      }
+    ]
+  ]
+};
--- src/app.js
+++ src/app.js
@@ -1,15 +1,8 @@
-import "./app.css";
-import nyancat from "./nyancat.jpeg";
+import MainController from "./controllers/MainController.js";
+
+// TODO: app.css 파일이 사라졌습니다. 이제는 app.scss를 불러올수 있도록 웹팩 구성을 추가하세요.
+import "./app.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.innerHTML = `
-        <img src="${nyancat}" />
-    `
-})
-
-console.log(process.env.NODE_ENV);
-
-console.log(VERSION);
-console.log(PRODUCTION);
-console.log(MAX_COUNT);
-console.log(api.domain);
+  new MainController();
+});
