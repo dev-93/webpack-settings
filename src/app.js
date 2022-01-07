d561417df2fdae0131880ed7542c61bb7baa57e4
kimtaenam <cjb940103@gmail.com>
2022-01-07 16:47:27 +0900

    webpack dev server add and use

--- src/app.js
+++ src/app.js
@@ -2,14 +2,7 @@ import "./app.css";
 import nyancat from "./nyancat.jpeg";
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.innerHTML = `
+  document.body.innerHTML = `
         <img src="${nyancat}" />
-    `
-})
-
-console.log(process.env.NODE_ENV);
-
-console.log(VERSION);
-console.log(PRODUCTION);
-console.log(MAX_COUNT);
-console.log(api.domain);
+    `;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     mode: 'development',
     entry: {
-        main: './app.js'
+        main: './src/app.js'
     },
     output: {
         path: path.resolve("./dist"),
