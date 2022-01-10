c625179f4db7c908e18b95268a6687cd9d41f0d2
kimtaenam <cjb940103@gmail.com>
2022-01-10 23:45:28 +0900

    webpack 최적화 실습 완료

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@ const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
   // TODO: 환경변수 NODE_ENV에 따라 development나 production 값을 설정하세요 
-  mode: "development",
+  mode,
   entry: {
     main: "./src/app.js"
   },
@@ -70,7 +75,30 @@ module.exports = {
     new CleanWebpackPlugin(),
     ...(process.env.NODE_ENV === "production"
       ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
-      : [])
-  ]
+      : []),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./node_modules/axios/dist/axios.min.js",
+          to: "./axios.min.js", // 목적지 파일에 들어간다
+        },
+      ]
+    }),
+  ],
+  optimization: {
+    minimizer: mode === "production" ? [
+        new CssMinimizerPlugin(),
+        new TerserPlugin({
+            terserOptions: {
+                compress: {
+                    drop_console: true, // 콘솔 로그를 제거한다
+                },
+            },
+        }),
+    ] : [],
+  },
+  externals: {
+    axios: "axios",
+  },
   // TODO: 여기에 최적화 설정을 구성하세요 
 };
